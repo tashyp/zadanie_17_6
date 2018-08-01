6ed17dbeaabd7574a95ef0b190a0f70f34193e86
Przemek Saja <przemek.saja@gmail.com>
2018-08-01 20:09:51 +0200

    Add configuration file and server

---
+++ server.js
@@ -0,0 +1,45 @@
+//server.js
+
+var express = require('express');
+var app = express();
+
+app.set('view engine', 'pug');
+app.set('views','./views');
+
+app.get('/', function (req, res) {
+    res.render('loginpage');
+});
+app.get('/auth/google', function (req, res) {
+    res.render('loggedpage');
+});
+
+/*
+app.use('/store', function(req, res, next){
+    console.log('Jestem pośrednikiem przy żądaniu do /store');
+    next();
+});
+
+app.get('/', function (req, res) {
+    res.send('Hello world!');
+});
+
+app.get('/store', function (req, res) {
+    res.send('To jest sklep');
+});
+
+app.get('/first-template', function(req, res){
+    res.render('first-template');
+});
+
+app.get('/dynamic-view', function(req, res){
+    res.render('dynamic', {
+        name: "Moja dynamiczna strona",
+        url: "http://www.google.com"
+    });
+});
+*/
+app.listen(3000);
+
+app.use(function (req, res, next) {
+    res.status(404).send('Wybacz, nie mogliśmy odnaleźć tego, czego żądasz!')
+});
